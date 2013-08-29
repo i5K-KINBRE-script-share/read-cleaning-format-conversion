ee7a7b1a162cfa0a70dd2a3456125b009c30330f
kstatebioinfo <kstate.bioinformatics@gmail.com>
2013-08-29 16:28:40 -0500

    Update fasta2fastq.pl

--- KSU_bioinfo_lab/fasta2fastq.pl
+++ KSU_bioinfo_lab/fasta2fastq.pl
@@ -1,18 +1,23 @@
 #!/usr/bin/env perl
 
-##### Script converts FAST and QAUL files to FASTQ format. It is a slight modification of a script posted to SeqAnwsers http://seqanswers.com/forums/showthread.php?t=2775 
-##### Usage: fasta2fastq [base filename (e.g. filename minus ".fasta"] 
+##### Script converts FASTA and QUAL files to FASTQ format. It is a slight modification of a script posted to SeqAnwsers http://seqanswers.com/forums/showthread.php?t=2775 
+##### Usage: fasta2fastq -b [base filename e.g. filename minus .fasta] -d [directory]
 
 use strict;
 use warnings;
+use Getopt::Long; # allows me to call arguments by name instead of order
+
+### import my arguments ###
+GetOptions(
+'b|base:s' => \$base,
+'d|directory:s' => \$dir) or die ""Usage: fasta2fastq -b [base filename e.g. filename minus .fasta] -d [directory]!\n";
 
 my $offset = 33; # I think this was 33 for sanger FASTQ, change this if required!
 my $count = 0;
 my %seqs;
-my $qual =$ARGV[0].".qual";
-my $fasta =$ARGV[0].".fasta";
-my $fastq =$ARGV[0].".fastq";
-die ("Usage: fasta2fastq [base filename (e.g. filename minus .fasta]") unless  (scalar @ARGV) == 1;
+my $qual =${dir}.${base}.".qual";
+my $fasta =${dir}.${base}.".fasta";
+my $fastq =${dir}.${base}.".fastq";
 
 open FASTA, $fasta or die "cannot open fasta: $!\n";
 open QUAL, $qual or die "cannot open qual: $!\n";
@@ -27,13 +32,11 @@ while (<FASTA>)
 		my ($fdef, @seqLines) = split /\n/;
 		my $seq = join '', @seqLines;
 		$seqs{$fdef} = $seq;
-#		print "$seqs{$fdef}\n";
 	}
 	$count++;
 }
 close FASTA;
 $count = 0;
-# my $qual0 = overrideDefault(33, 'qual_zero');
 my $qual0 =33;
 while (<QUAL>)
 {
