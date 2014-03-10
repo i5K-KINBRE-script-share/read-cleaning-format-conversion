2e45e9f222fc354f46556c7c31b2942c68b84e2f
Jennifer Shelton <kstate.bioinformatics@gmail.com>
2014-03-10 10:54:50 -0500

    update contigs from fasta

---
+++ KSU_bioinfo_lab/get_seq_by_id.pl
@@ -0,0 +1,30 @@
+#!/bin/perl
+##################################################################################
+#   
+#	USAGE: perl get_seq_by_id.pl [header or headers separated by spaces]
+#
+#  Created by jennifer shelton
+#	Script creates new fasta files including all sequences corresponding to the headers given as input
+#  perl get_seq_by_id.pl tcas_3770
+##################################################################################
+use strict;
+use warnings;
+use Bio::Seq;
+use Bio::SeqIO;
+use Bio::DB::Fasta; #makes a searchable db from my fasta file
+# use List::Util qw(max);
+# use List::Util qw(sum);
+##################################################################################
+##############                      notes                       ##################
+##################################################################################
+my $infile_fasta = "sample.fasta"; # give the name of your file
+my $out = "seq_got_by_id.fasta";
+
+my $db = Bio::DB::Fasta->new("$infile_fasta");
+my $seq_out = Bio::SeqIO->new('-file' => ">$out",'-format' => 'fasta');		#Create new fasta outfile object.
+
+for my $header (@ARGV)
+{
+	my $seq_obj = $db->get_Seq_by_id($header); #get fasta file
+	$seq_out->write_seq($seq_obj);
+}
--- KSU_bioinfo_lab/make_contigs_from_fasta.pl
+++ KSU_bioinfo_lab/make_contigs_from_fasta.pl
@@ -3,11 +3,12 @@
 use strict;
 use warnings;
 
-# this script breaks scaffolds at Ns and outputs a new multi fasta file with one entry pre contig 
+# this script breaks scaffolds at Ns and outputs a new multi fasta file with one entry per contig and an agp of the new contigs created from the scaffolds
 # USAGE: perl make_contigs_from_fasta.pl [fasta]
 
 open FASTA, '<', $ARGV[0] or die "Couldn't open $ARGV[0]: $!";
 open CONTIGFA, '>', "$ARGV[0]"."_contig.fasta" or die "Couldn't create $ARGV[0]_contig.fasta: $!";
+open AGP, '>', "$ARGV[0]"."_contig.agp" or die "Couldn't create $ARGV[0]_contig.agp: $!";
 
 
 my $counter=-1;
@@ -21,7 +22,10 @@ while (<FASTA>)
         my ($header,@seq)=split/\n/;
         my $seq=join '', @seq;
         $seq =~ s/>//g;
-        my @contigs= split(/N+/,$seq);
+        my @contigs= split(/N+/i,$seq);
+        my @gaps=split(/[AGCT]+/i,$seq);
+        my $gap_count = scalar (@gaps);
+        print "GAP COUNT: $gap_count\n";
         my $contig_counter=1;
         foreach my $broken (@contigs)
         {
