bcf8923ccbd62e174897fca5b7fecad658f4be51
Jennifer Shelton <kstate.bioinformatics@gmail.com>
2014-05-27 12:42:31 -0500

    add SeqByInterval.pl

---
+++ KSU_bioinfo_lab/SeqByInterval.pl
@@ -0,0 +1,49 @@
+#!/usr/bin/perl
+###############################################################################
+#
+#	USAGE: perl SeqByInterval.pl [first sequence number] [second sequence number] [fasta]
+#
+#	Description: Takes two numbers "x" and "y" as input and creates a new file with only sequence number "x" and sequence number "y" from the original fasta file. For example "perl SeqByInterval.pl 1 3 test.fasta" would create a new fasta with only the first three fasta entries.
+#  Created by jennifer shelton
+#
+###############################################################################
+use strict;
+use warnings;
+use File::Basename; # enable manipulating of the full path
+# use List::Util qw(max);
+# use List::Util qw(sum);
+use Bio::SeqIO;
+use Bio::Seq;
+use Bio::DB::Fasta;
+###############################################################################
+##############                Initialize variables           ##################
+###############################################################################
+# my $dirname = dirname(__FILE__);
+my $start = $ARGV[0];
+my $stop = $ARGV[1];
+my $fasta_in = $ARGV[2];
+my (${filename}, ${directories}, ${suffix}) = fileparse($fasta_in, '\..*');
+my $fasta_out = "${directories}${filename}_interval.fa";
+my $count = 1;
+###############################################################################
+##############          Open output fasta                    ##################
+###############################################################################
+my $seq_out = Bio::SeqIO->new(-file => ">$fasta_out", -format => 'fasta');
+###############################################################################
+##############          Open input fasta                    ##################
+###############################################################################
+my $seq_in = Bio::SeqIO->new(-file => "<$fasta_in", -format => 'fasta');
+###############################################################################
+##############                      Run                      ##################
+###############################################################################
+while (my $seq = $seq_in->next_seq)
+{
+    if (($count >= $start) && ($count <= $stop))
+    {
+        $seq_out->write_seq($seq);
+    }
+    ++$count;
+}
+
+print "done\n";
+
--- KSU_bioinfo_lab/make_contigs_from_fasta.pl
+++ KSU_bioinfo_lab/make_contigs_from_fasta.pl
@@ -3,7 +3,7 @@
 use strict;
 use warnings;
 
-# this script breaks scaffolds at Ns and outputs a new multi fasta file with one entry per contig and an agp of the new contigs created from the scaffolds
+# this script breaks scaffolds at Ns and outputs a new multi fasta file with one entry per contig and creates an agp for this
 # USAGE: perl make_contigs_from_fasta.pl [fasta]
 
 open FASTA, '<', $ARGV[0] or die "Couldn't open $ARGV[0]: $!";
@@ -13,6 +13,7 @@ open AGP, '>', "$ARGV[0]"."_contig.agp" or die "Couldn't create $ARGV[0]_contig.
 
 my $counter=-1;
 my $sum=0;
+my $tcas_id = 1;
 $/=">";
 while (<FASTA>)
 {
@@ -24,18 +25,37 @@ while (<FASTA>)
         $seq =~ s/>//g;
         my @contigs= split(/N+/i,$seq);
         my @gaps=split(/[AGCT]+/i,$seq);
-        my $gap_count = scalar (@gaps);
+        my $gap_count = (scalar(@gaps)-1);
         print "GAP COUNT: $gap_count\n";
-        my $contig_counter=1;
+        for my $gap (@gaps)
+        {
+            #            print "Gap: $gap\n\n";
+        }
+        my $gap_counter=1;
+        my $pos = 1;
+        my $agp_element=1;
         foreach my $broken (@contigs)
         {
-            print CONTIGFA ">${header}|Scaffold${counter}|Contig${contig_counter}\n";
+            print CONTIGFA ">tcas_${tcas_id}".'[organism=Triboliumcastaneum][strain=GeorgiaGA2][country=USA:Kansas][collection-date=Apr-2003]'."\n";
+            #            >tcas_1000[organism=Triboliumcastaneum][strain=GeorgiaGA2][country=USA:Kansas][collection-date=Apr-2003]
             print CONTIGFA "$broken\n";
-            ++$contig_counter;
+            my $contig_length = length($broken);
+            my $stop = $pos + $contig_length - 1;
+            print AGP "${header}\t$pos\t$stop\t$agp_element\tW\ttcas_${tcas_id}\t1\t${contig_length}\t+\n";
+            ++$agp_element;
+            if ($gaps[$gap_counter])
+            {
+                $pos = $stop + 1;
+                my $gap_length = length($gaps[$gap_counter]);
+                $stop = $pos + $gap_length - 1;
+                print AGP "${header}\t$pos\t$stop\t$agp_element\tN\t${gap_length}\tscaffold\tyes\tunspecified\n";
+                ++$agp_element;
+                ++$gap_counter;
+            }
+            ++$tcas_id;
+            $pos = $stop + 1;
         }
-
+        
     }
     
 }
-
-
