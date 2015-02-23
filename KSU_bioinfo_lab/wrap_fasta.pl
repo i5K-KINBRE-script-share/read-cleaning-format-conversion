149644ff768e612a3e9c7b2f3b2156e5dc8c6f04
Jennifer Shelton <kstate.bioinformatics@gmail.com>
2015-02-23 16:11:45 -0600

    Changed wrap_fasta.pl output
    Changed wrap_fasta.pl to output files with "_wrapped.fasta" suffix

--- KSU_bioinfo_lab/wrap_fasta.pl
+++ KSU_bioinfo_lab/wrap_fasta.pl
@@ -4,7 +4,7 @@
 #	USAGE: perl wrap_fasta.pl
 #
 #  Created by jennifer shelton
-#  DESCRIPTION: Wraps fasta files to the NCBI recommended 80 bp per line https://www.ncbi.nlm.nih.gov/BLAST/blastcgihelp.shtml. Does not require BioPerl. 
+#  DESCRIPTION: Wraps fasta files to the NCBI recommended 80 bp per line https://www.ncbi.nlm.nih.gov/BLAST/blastcgihelp.shtml. Does not require BioPerl. scripts that wraps lines of fasta file without wrapped sequences or with inconsistently wrapped sequences. NCBI recommends that FASTA sequences be wrapped and BioPerl requires this however not all tools create wrapped FASTA files. Your reformatted fasta file will start with the original filename and end with "_wrapped.fasta".
 ##################################################################################
 use strict;
 use warnings;
@@ -18,7 +18,7 @@ use File::Basename; # enable maipulating of the full path
 my $fasta = $ARGV[0];
 open (FASTA, "<", $fasta) or die  "can't open $fasta ! \n";
 my (${filename}, ${directories}, ${suffix}) = fileparse($fasta,'\..*');
-my $out_fasta = "${directories}${filename}_wrap.fasta";
+my $out_fasta = "${directories}${filename}_wrapped.fasta";
 open (OUTFASTA,">","$out_fasta") or die "can't open $out_fasta\n";
 while (<FASTA>)
 {
