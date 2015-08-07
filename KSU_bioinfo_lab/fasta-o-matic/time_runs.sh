d3e0d0478228c6e8fa396c4a84c47e5540825bd5
Jennifer Shelton <kstate.bioinformatics@gmail.com>
2015-08-07 09:17:07 -0700

    Test time etc

--- KSU_bioinfo_lab/fasta-o-matic/time_runs.sh
+++ KSU_bioinfo_lab/fasta-o-matic/time_runs.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/bash
-start_time=`date +%s`
+echo "Hello"
+echo "Wrap only Fasta-O-Matic execution:"
+#526585/memusg python ~/read-cleaning-format-conversion/KSU_bioinfo_lab/fasta-o-matic/fasta_o_matic.py -f ~/test_fasta_o_matic/input/GCF_000164845.1_Vicugna_pacos-2.0.1_genomic_FAKE.fna -o ~/test_fasta_o_matic/out -c -s wrap -q
 
-python ~/read-cleaning-format-conversion/KSU_bioinfo_lab/fasta-o-matic/fasta_o_matic.py -f ~/test_fasta_o_matic/input/GCF_000164845.1_Vicugna_pacos-2.0.1_genomic_FAKE.fna -o ~/test_fasta_o_matic/out -c -s wrap
+/usr/bin/time -v python ~/read-cleaning-format-conversion/KSU_bioinfo_lab/fasta-o-matic/fasta_o_matic.py -f ~/test_fasta_o_matic/input/GCF_000164845.1_Vicugna_pacos-2.0.1_genomic_FAKE.fna -o ~/test_fasta_o_matic/out -c -s wrap -q
 
-end_time=`date +%s`
-echo execution time was `expr $end_time - $start_time` s.
-
-python ~/read-cleaning-format-conversion/KSU_bioinfo_lab/fasta-o-matic/fasta_o_matic.py -f ~/test_fasta_o_matic/input/GCF_000164845.1_Vicugna_pacos-2.0.1_genomic_FAKE.fna -o ~/test_fasta_o_matic/out1 -c
+echo "All steps Fasta-O-Matic execution:"
+#526585/memusg python ~/read-cleaning-format-conversion/KSU_bioinfo_lab/fasta-o-matic/fasta_o_matic.py -f ~/test_fasta_o_matic/input/GCF_000164845.1_Vicugna_pacos-2.0.1_genomic_FAKE.fna -o ~/test_fasta_o_matic/out1 -c -q
+/usr/bin/time -v python ~/read-cleaning-format-conversion/KSU_bioinfo_lab/fasta-o-matic/fasta_o_matic.py -f ~/test_fasta_o_matic/input/GCF_000164845.1_Vicugna_pacos-2.0.1_genomic_FAKE.fna -o ~/test_fasta_o_matic/out1 -c -q
+echo "Wrap only setk execution:"
+#526585/memusg /home/bionano/seqtk/seqtk seq -l 60 /home/bionano/test_fasta_o_matic/input/GCF_000164845.1_Vicugna_pacos-2.0.1_genomic_FAKE.fna > /home/bionano/test_fasta_o_matic/out2/GCF_000164845.1_Vicugna_pacos-2.0.1_genomic_FAKE_seqtk_wrap.fna
+/usr/bin/time -v /home/bionano/seqtk/seqtk seq -l 60 /home/bionano/test_fasta_o_matic/input/GCF_000164845.1_Vicugna_pacos-2.0.1_genomic_FAKE.fna > /home/bionano/test_fasta_o_matic/out2/GCF_000164845.1_Vicugna_pacos-2.0.1_genomic_FAKE_seqtk_wrap.fna
+echo "Wrap only seqret execution:"
+#526585/memusg /home/bionano/EMBOSS-6.5.7/emboss/seqret -sequence ~/test_fasta_o_matic/input/GCF_000164845.1_Vicugna_pacos-2.0.1_genomic_FAKE.fna -outseq /home/bionano/test_fasta_o_matic/out3/GCF_000164845.1_Vicugna_pacos-2.0.1_genomic_FAKE_seqret.fna
+/usr/bin/time -v /home/bionano/EMBOSS-6.5.7/emboss/seqret -sequence ~/test_fasta_o_matic/input/GCF_000164845.1_Vicugna_pacos-2.0.1_genomic_FAKE.fna -outseq /home/bionano/test_fasta_o_matic/out3/GCF_000164845.1_Vicugna_pacos-2.0.1_genomic_FAKE_seqret.fna
+ 
